e5968342f2ddaa37d33b1bd11e0ca4bf2d51e1e8
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2022-10-14 02:20:04 -0500

    registro y productos agregados

---
+++ InnovaAqp/src/java/Modelo/Dao/DaoAccesorios.java
@@ -0,0 +1,42 @@
+package Modelo.Dao;
+
+import Modelo.Accesorio;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaoAccesorios extends Conexion{
+    public List<Accesorio> consultarTodos() {
+        List<Accesorio> lst = new ArrayList<Accesorio>();
+        Connection cnx = obtenerConexion();
+        String sql = "SELECT * FROM productos";
+        try {
+            PreparedStatement stm = cnx.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {                
+                Accesorio ac = new Accesorio();
+                ac.setNombre(rs.getString(2));
+                ac.setPeso(rs.getDouble(3));
+                ac.setMarca(rs.getString(4));
+                ac.setTipo(rs.getString(5));
+                ac.setDescripcion(rs.getString(6));
+                ac.setPrecio(rs.getDouble(7));
+                ac.setFechFabricacion(rs.getDate(8));
+                ac.setImagen(rs.getString(9));
+                lst.add(ac);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        finally{
+            try {
+                cnx.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return lst;
+    }
+}
